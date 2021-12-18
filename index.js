3d44bcb0abec320d79bdb4ae4b0922f80b60d4a7
Igor <i_swat@mail.ru>
2021-12-18 16:50:24 +0300

    Урок 3. Работа с файловой системой. Класс Buffer. Модуль Streams
    Разбор методички.
    до потока на запись

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
-// console.log('Hello World!');
+//разбор методички
 
-const colors = require('colors/safe')
+const fs = require('fs')
 
-console.log(colors.red('Hello World!'));
+//*************** Чтение файла ***************
+
+//Асинхронное чтение файла
+// fs.readFile('./access.log', (err, data) => console.log(data.toString('utf-8')))
+
+
+//Синхронное чтение файла
+// const data = fs.readFileSync('./access.log');
+// const data = fs.readFileSync('./access.log', 'utf-8')
+//
+// console.log(data);
+
+//*************** Запись файла ***************
+
+// const log1 = '127.0.0.1 - - [30/Jan/2021:11:11:20 -0300] "POST /foo HTTP/1.1" 200 0 "-" "curl/7.47.0"';
+// const log2 = '127.0.0.1 - - [30/Jan/2021:11:11:25 -0300] "GET /boo HTTP/1.1" 404 0 "-" "curl/7.47.0"';
+
+//->Асинхронная запись файла
+// Файл перезаписывается
+// fs.writeFile('./access.log', log1, (err) => {
+//     if (err == null) console.log('Запись в файл произошла успешно')
+//     else console.log('Ошибка при записи в файл', err)
+// });
+
+// fs.writeFile('./access.log', `\n${log1}`, {flag: 'a'}, (err) => {
+//     if (err == null) console.log('Запись в файл произошла успешно')
+//     else console.log('Ошибка при записи в файл', err)
+// })
+
+//->Синхронная запись файла
+
+//Файл перезаписывается
+// fs.writeFileSync('./access.log', log2);
+
+// fs.writeFileSync('./access.log', `\n${log2}`, {flag: 'a'}, (err) => console.log(err))
+
+//*************** Поток на чтение ***************
+// const readStream = fs.createReadStream('./access.log', 'utf-8');
+// readStream.on('data', (chunk) => {
+//     console.log('Chunk');
+//     console.log(chunk);
+// })
+// readStream.on('end', () => console.log('Чтение файла завершено!'))
+// readStream.on('error', () => console.log(err))
